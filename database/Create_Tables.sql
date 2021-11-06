62ad691cad09a34011aae664c2a87e8d0576c34f
ramonmsa <ramonmoraissa@gmail.com>
2021-11-06 00:01:12 -0400

    update file Create_tables.sql with the script to create table confirmedcases

---
+++ database/Create_Tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE confirmedcases ( 
+----------
+Row_ID                      INTEGER UNIQUE PRIMARY KEY, -- this row is already present in the dataset.
+Accurate_Episode_Date       DATE,
+Case_Reported_Date          DATE,
+Test_Reported_Date          DATE,
+Specimen_Date               DATE,
+Age_Group                   VARCHAR(7),
+Client_Gender               VARCHAR(11),
+Case_AcquisitionInfo        VARCHAR(30),
+Outcome1                    VARCHAR(30),
+Outbreak_Related            BOOLEAN,
+Reporting_PHU_ID            INTEGER,
+Reporting_PHU               VARCHAR(100),
+Reporting_PHU_Address       VARCHAR(100),
+Reporting_PHU_City          VARCHAR(30),
+Reporting_PHU_Postal_Code   VARCHAR(7),
+Reporting_PHU_Website       VARCHAR(100),
+Reporting_PHU_Latitude      DECIMAL(10,7), --Latitudes range from -90 to +90
+Reporting_PHU_Longitude     DECIMAL(11,7)  --longitudes range from -180 to +180
+)
